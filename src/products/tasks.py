f7de5fc5e8c5b5161d82d4aefef07e1dec29163b
pharci <57046184+pharci@users.noreply.github.com>
2025-09-09 01:44:24 +0300

    Update tasks.py

--- src/products/tasks.py
+++ src/products/tasks.py
@@ -1,5 +1,5 @@
 from celery import shared_task
-from products.MS.sync_inventory import sync_inventory
+from products.integrations.sync_inventory import sync_inventory
 
 @shared_task(bind=True, autoretry_for=(Exception,), retry_backoff=True, max_retries=5)
 def update_inventory_minutely(self):
